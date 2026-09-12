262f63b16cd93091690c750ac429b1578f3e7206
HunterAllen06 <77126372+HunterAllen06@users.noreply.github.com>
2026-09-12 14:22:20 -0700

    Pretty major update to player mover ground detection

--- Runtime/PlayerMover.cs
+++ Runtime/PlayerMover.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -65,6 +66,11 @@ namespace HunterAllen.Player
         RaycastHit[] _groundHitResults = new RaycastHit[4];
         RaycastHit[] _headCheckResults = new RaycastHit[1];
 
+        void Start()
+        {
+            _maxSlopeDot = 1f - (MaxSlopeAngle / 90f);
+        }
+        
         void Update()
         {
             HandleCrouchInput();
@@ -89,48 +95,70 @@ namespace HunterAllen.Player
 
             float multiplier = IsSprinting || OverrideIsSprinting ? DirectionBiasSprintMultiplier : 1f;
 
-            Ray ray = new(Collider.transform.position - (Collider.height * 0.5f - Collider.radius) * Vector3.up + DirectionBias * multiplier * direction.normalized, Vector3.down);
+            Vector3 rayOrigin = Collider.transform.position - 0.5f * Collider.height * Vector3.up;
+            Vector3 rayOriginWithRadiusOffset = rayOrigin + (Collider.radius * Vector3.up);
+            Ray ray = new(rayOrigin, Vector3.down);
+            Ray rayWithDirectionBias = new(rayOriginWithRadiusOffset + DirectionBias * multiplier * direction.normalized, Vector3.down);
 
-            int hits = Physics.SphereCastNonAlloc(ray, Collider.radius * SpringRadius, _groundHitResults, SpringRaycastDistance, GroundLayer, QueryTriggerInteraction.Ignore);
+            int hits = Physics.SphereCastNonAlloc(rayWithDirectionBias, Collider.radius * SpringRadius, _groundHitResults, SpringRaycastDistance, GroundLayer, QueryTriggerInteraction.Ignore);
             if (hits == 0) return;
 
-            // Select hit closest to the feet
-            RaycastHit groundHit = default;
+            RaycastHit groundHit;
+            bool onGround = Physics.Raycast(ray, out groundHit, SpringRaycastDistance, GroundLayer, QueryTriggerInteraction.Ignore);
+
+            Debug.DrawRay(ray.origin, SpringRaycastDistance * ray.direction, onGround ? Color.green : Color.red);
+            Debug.DrawRay(rayWithDirectionBias.origin - Collider.radius * Vector3.up, SpringRaycastDistance * rayWithDirectionBias.direction, direction.magnitude == 0 ? Color.green : Color.yellow);
+
+            var colliderPosition = Collider.transform.position;
+            var feetPosition = ray.origin - SpringHeight * Vector3.up;
             float previousHorizontalDistance = SpringRaycastDistance;
+            float previousHitHeight = SpringHeight - SpringRaycastDistance;
+            
+            // Select hit closest to the feet
             for (int i = 0; i < hits; i++)
             {
                 var hit = _groundHitResults[i];
+                var currentHorizontalDistance = (new Vector2(hit.point.x, hit.point.z) - new Vector2(colliderPosition.x, colliderPosition.z)).magnitude;
+                var hitHeight = hit.point.y - feetPosition.y;
 
                 if (hit.point == default || Tags.Contains(hit.collider.tag) == TagsAsBlacklist)
                 {
+                    StartCoroutine(DrawPoint(hit.point, 1));
                     continue;
                 }
-                if (groundHit.point == default)
+                StartCoroutine(DrawPoint(hit.point, 2));
+                if (groundHit.point == default && hitHeight < MaxStepHeight + 0.05f)
                 {
                     // Because of the direction bias setting, I'd like to do a separate raycast for the
                     // default grouind hit instead of the first hit in the array
-                    groundHit = _groundHitResults[i];
+                    groundHit = hit;
                     continue;
                 }
 
-                var colliderPosition = Collider.transform.position;
-                var currentHorizontalDistance = (new Vector2(hit.point.x, hit.point.z) - new Vector2(colliderPosition.x, colliderPosition.z)).magnitude;
-                var hitHeight = ray.origin.y - hit.point.y; // Collider.transform.position.y - Collider.height - SpringHeight;
+                bool isStepAboveFeet = hitHeight >= 0;
+                bool isLessThanStepHeight = hitHeight < MaxStepHeight + 0.05f;
+                bool isLessThanMaxSlope = Vector3.Dot(hit.normal, Vector3.up) > _maxSlopeDot;
+                bool isClosestHorizontally = currentHorizontalDistance < previousHorizontalDistance;
+                bool isHighestVertically = hitHeight > previousHitHeight;
+                // need to figure out conditions for step up vs step down
 
-                if (
-                    hitHeight > SpringHeight && // Don't set hit if below 'feet'
-                    hitHeight < MaxStepHeight + 0.05f && // Step height
-                    Vector3.Dot(hit.normal, Vector3.up) > _maxSlopeDot && // Slope
-                    currentHorizontalDistance < previousHorizontalDistance) // Closest to player horizontally
-                                                                            //hit.distance < groundHit.distance &&
+                if (isLessThanStepHeight && isLessThanMaxSlope && isHighestVertically) // && isClosestHorizontally)
                 {
                     groundHit = hit;
+                    // previousHorizontalDistance = currentHorizontalDistance;
+                    previousHitHeight = hitHeight;
                 }
             }
 
+            if (groundHit.point == default) return;
+            StartCoroutine(DrawPoint(groundHit.point, 3));
+
+            // var distance = SpringHeight - groundHit.distance;
+            var distance = SpringHeight - (ray.origin.y - groundHit.point.y);
+            float velocityDot = Vector3.Dot(velocity, Vector3.up);
             multiplier = IsSprinting || OverrideIsSprinting ? SpringForceSprintMultiplier : 1f;
-            float force = (SpringHeight - groundHit.distance) * SpringForce * multiplier - (velocity.y * SpringDamp);
-            Rigidbody.AddForce(force * Time.fixedDeltaTime * Vector3.up);
+            float force = (distance) * SpringForce * multiplier - (velocityDot * SpringDamp) + Rigidbody.mass * Physics.gravity.magnitude;
+            Rigidbody.AddForce(force * Vector3.up, ForceMode.Force);
         }
         void ApplyMovementForce(Vector2 input)
         {
@@ -165,12 +193,71 @@ namespace HunterAllen.Player
 
         void OnValidate()
         {
+            if (Application.isPlaying) return;
+
             Collider.height = DefaultHeight;
             Vector3 newPos = Collider.transform.position;
             newPos.y = Collider.height * 0.5f + SpringHeight;
             Collider.transform.position = newPos;
             CameraTransform.localPosition = (DefaultHeight * 0.5f - 0.1f) * Vector3.up;
             _maxSlopeDot = 1f - (MaxSlopeAngle / 90f);
+        }
+
+        IEnumerator DrawPoint(Vector3 pos, int index)
+        {
+            Color c = index switch
+            {
+                1 => Color.red,
+                2 => Color.yellow,
+                3 => Color.green,
+                _ => Color.beige,
+            };
+
+            float scale = index switch
+            {
+                1 => 0.3f,
+                2 => 0.2f,
+                3 => 0.1f,
+                _ => 0.1f,
+            };
+            var backBottomLeft = pos - scale * 0.5f * Vector3.one;
+            var backBottomRight = backBottomLeft + scale * Vector3.right;
+            var frontTopRight = pos + scale * 0.5f * Vector3.one;
+            var frontTopLeft = frontTopRight - scale * Vector3.right;
+            var backTopLeft = frontTopLeft - scale * Vector3.forward;
+            var backTopRight = frontTopRight - scale * Vector3.forward;
+            var frontBottomRight = backBottomRight + scale * Vector3.forward;
+            var frontBottomLeft = backBottomLeft + scale * Vector3.forward;
+
+            float time = index switch
+            {
+                1 => 2f,
+                2 => 0.5f,
+                _ => 0.2f,
+            };
+            float t = time;
+
+            while (time > 0f)
+            {
+                c = Color.Lerp(c, c - Color.black, t - time);
+                Debug.DrawLine(backBottomLeft, backBottomRight, c);
+                Debug.DrawLine(backBottomLeft, backTopLeft, c);
+                Debug.DrawLine(backTopRight, backTopLeft, c);
+                Debug.DrawLine(backTopRight, backBottomRight, c);
+
+                Debug.DrawLine(frontBottomLeft, frontBottomRight, c);
+                Debug.DrawLine(frontBottomLeft, frontTopLeft, c);
+                Debug.DrawLine(frontTopRight, frontTopLeft, c);
+                Debug.DrawLine(frontTopRight, frontBottomRight, c);
+
+                Debug.DrawLine(backBottomLeft, frontBottomLeft, c);
+                Debug.DrawLine(backTopLeft, frontTopLeft, c);
+                Debug.DrawLine(backBottomRight, frontBottomRight, c);
+                Debug.DrawLine(backTopRight, frontTopRight, c);
+
+                time -= Time.deltaTime;
+                yield return null;
+            }
         }
     }
 }
